62424e85e9a6df391c71fce9e643d1cc8332ce26
jeonnni <ymymda0711@gmail.com>
2025-12-16 06:11:23 +0900

    getCurrentPosition

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,20 @@
 // 날씨
 
+import { API_KEY } from "./config.js"
+
 function onGeoOk(position){ 
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(lat,lng);
-    
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(data=> {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerHTML = data.name; // 위치
+        weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}` // 날씨
+    })
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.")
